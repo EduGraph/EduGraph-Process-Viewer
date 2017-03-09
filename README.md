50e332738d9d785eedc83227d4b8a18c55200b24
Jonas Jetschni <jetschni@gmail.com>
2017-03-09 18:21:49 +0100

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# EduGraph Process Viewer
+
+Based on the [simple-bower](https://github.com/bpmn-io/bpmn-js-examples/tree/master/simple-bower) and [overlays](https://github.com/bpmn-io/bpmn-js-examples/tree/master/overlays) examples.
+
+## License
+
+MIT
---
+++ index-async.js
@@ -0,0 +1,44 @@
+/**
+ * bpmn-js-seed - async
+ *
+ * This is an example script that loads a bpmn diagram <diagram.bpmn> and opens
+ * it using the bpmn-js viewer.
+ *
+ * YOU NEED TO SERVE THIS FOLDER VIA A WEB SERVER (i.e. Apache) FOR THE EXAMPLE TO WORK.
+ * The reason for this is that most modern web browsers do not allow AJAX requests ($.get and the like)
+ * of file system resources.
+ */
+(function(BpmnViewer, $) {
+
+  // create viewer
+  var bpmnViewer = new BpmnViewer({
+    container: '#canvas'
+  });
+
+
+  // import function
+  function importXML(xml) {
+
+    // import diagram
+    bpmnViewer.importXML(xml, function(err) {
+
+      if (err) {
+        return console.error('could not import BPMN 2.0 diagram', err);
+      }
+
+      var canvas = bpmnViewer.get('canvas'),
+          overlays = bpmnViewer.get('overlays');
+
+
+      // zoom to fit full viewport
+      canvas.zoom('fit-viewport');
+
+    });
+  }
+
+
+  // load external diagram file via AJAX and import it
+  $.get('edu-graph.bpmn', importXML, 'text');
+
+
+})(window.BpmnJS, window.jQuery);
